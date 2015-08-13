6cb1c54427c16a4957abbc442594033b403e1b01
oricalvo <ori.calvo@gmail.com>
2015-08-13 14:31:31 +0300

    More

---
+++ Samples@class/02 Introduction to AngularJS/GettingStarted/GettingStarted/HomeCtrl.js
@@ -0,0 +1,29 @@
+﻿(function () {
+    "use strict";
+
+    function HomeCtrl($scope) {
+
+        $scope.message = "Hello Angular";
+
+        $scope.add = function () {
+            if (!$scope.name) {
+                return;
+            }
+
+            var contact = {
+                id: -1,
+                name: $scope.name
+            };
+
+            $scope.contacts.push(contact);
+        }
+
+        $scope.contacts = [
+            { id: 1, name: "Ori" },
+            { id: 2, name: "Roni" },
+        ];
+    }
+
+    angular.module("MyApp").controller("HomeCtrl", HomeCtrl);
+
+})();
